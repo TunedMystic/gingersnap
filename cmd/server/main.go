ba2d32aa5a8d2123ce035065a51f4d99648fe281
Sandeep Jadoonanan <sandeep+tech@sandeep.net>
2023-09-15 17:37:05 -0400

    Modify the server cmd to watch for source code changes. This will only be used for the development of the gingersnap cli, not the usage of it.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"gingersnap"
+	"log"
+
+	"github.com/fsnotify/fsnotify"
 )
 
 func main() {
@@ -9,13 +12,64 @@ func main() {
 		ConfigPath: "assets/config/gingersnap.json",
 		PostsGlob:  "assets/posts/*.md",
 		MediaDir:   "assets/media",
+		Debug:      true,
 	}
 
 	g := gingersnap.New()
 	g.Configure(s)
 
-	go g.RunServerWithWatcher(s)
+	go RunServerWithWatcher(g, s)
 
 	// Block main goroutine forever.
 	<-make(chan struct{})
+}
+
+// RunServerWithWatcher runs the server and and watches for file changes.
+// On file change, it resets the gingersnap engine and restarts the server.
+// .
+func RunServerWithWatcher(g *gingersnap.Gingersnap, s gingersnap.Settings) {
+	// Create new watcher.
+	w, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	paths := []string{
+		"gingersnap.go",
+		"assets/config",
+		"assets/css",
+		"assets/media",
+		"assets/posts",
+		"assets/templates",
+	}
+
+	for _, path := range paths {
+		if err = w.Add(path); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	g.Logger.Printf("Watching for file changes")
+
+	go g.RunServer()
+
+	for {
+		select {
+		case event, ok := <-w.Events:
+			if !ok {
+				return
+			}
+			if event.Has(fsnotify.Write) || event.Has(fsnotify.Create) || event.Has(fsnotify.Rename) {
+				g.Logger.Println("Files changed. Restarting server")
+
+				g.Configure(s)
+				go g.RunServer()
+			}
+		case err, ok := <-w.Errors:
+			if !ok {
+				return
+			}
+			g.Logger.Println("error:", err)
+		}
+	}
 }
