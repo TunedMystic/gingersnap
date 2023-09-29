d216e5f304cf83cfd2330add01575ee581f0b181
Sandeep Jadoonanan <sandeep+tech@sandeep.net>
2023-09-29 03:53:33 -0400

    Create temporary command to convert all images in the media directory to webp

---
+++ cmd/webp/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"gingersnap"
+)
+
+func Convert(paths []string) error {
+
+	for _, path := range paths {
+		srcPath := path
+		dstPath := strings.ReplaceAll(srcPath, filepath.Ext(path), ".webp")
+		fmt.Printf("✨🏞️ ✨converting %s to webp\n", srcPath)
+
+		cmd := []string{"./bin/cwebp", "-q", "85", srcPath, "-o", dstPath}
+
+		// Convert the image to webp.
+		if _, err := exec.Command(cmd[0], cmd[1:]...).Output(); err != nil {
+			return fmt.Errorf("failed webp conversion: %w", err)
+		}
+
+		// Remove the non-webp image.
+		if err := os.Remove(srcPath); err != nil {
+			return fmt.Errorf("failed to remove original image: %w", err)
+		}
+	}
+
+	return nil
+}
+
+// ------------------------------------------------------------------
+//
+//
+// Main entrypoint
+//
+//
+// ------------------------------------------------------------------
+
+func main() {
+	paths := make([]string, 0, 10)
+	globs := []string{
+		"assets/media/**/*.png",
+		"assets/media/**/*.jpg",
+		"assets/media/**/*.jpeg",
+	}
+
+	for _, pattern := range globs {
+		names, err := gingersnap.Glob(os.DirFS("."), pattern)
+		if err != nil {
+			log.Fatal(err)
+		}
+		paths = append(paths, names...)
+	}
+
+	if err := Convert(paths); err != nil {
+		log.Fatal(err)
+	}
+}
